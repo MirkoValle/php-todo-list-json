e1796c02c9b831d4e360d78d4d73b338b7782dca
MirkoValle <mirko.valle20@gmail.com>
2024-07-01 22:35:51 +0200

    Added css

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
+    <!-- Fontawesome 6.5.2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
     <main id="app">
@@ -18,28 +22,23 @@
 
     <section class="todo">
         <ul>
-            <li v-for="(todo, index) in todoList">
-                <p> {{ todo.name }}</p>
-                <form action="./server.php" method="POST">
-                    <button name="delete" :value="index" type="submit">X</button>
+            <li v-for="(todo, index) in todoList" :class="(todo.completed == true) ? 'completed' : '' ">
+                <p @click="todo.completed = !todo.completed"> {{ todo.name }}</p>
+                <form class="delete" action="./server.php" method="POST">
+                    <button class="delete" name="delete" :value="index" type="submit"><i class="fa-solid fa-trash-can"></i></button>
                 </form>
             </li>
         </ul>
     </section>
+
     <section class="input">
         <form action="./server.php" method="POST">
-        <input type="text" name="add" id="add" placeholder="Aggiungi una nuova task">
+        <input tabindex="0" type="text" name="add" id="add" placeholder="Aggiungi una nuova task">
+        <button class="input" type="submit"><i class="fa-solid fa-plus"></i></button>
         </form>
     </section>
 
-
-
-
-
-
-
     </main>
-
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="./js/script.js"></script>
 </body>
--- server.php
+++ server.php
@@ -5,20 +5,26 @@ header("Content-type: application/json");
 $getRawData = file_get_contents("./db/list.json");
 $data = json_decode($getRawData, true);
 
+// Funzione per aggiungere una task
 if(isset($_POST['add']) && strlen($_POST['add']) > 0) {
 
     $data[] = [
-        "name" => $_POST['add'],
+        "name" => "- " . ucfirst($_POST['add']),
         "completed" => false
     ];
 
     header('location: ./index.php');
 }
 
+if(isset($_POST['add']) && strlen($_POST['add']) == 0){
+    header('location: ./index.php'); 
+}
+
+// Funzione per eliminare una task
 if(isset($_POST['delete'])) {
 
     unset($data[$_POST['delete']]);
-    
+
     header('location: ./index.php');
 }
 
@@ -27,4 +33,5 @@ $data = json_encode($data);
 file_put_contents('./db/list.json', $data);
 
 echo ($data)
+
 ?>
